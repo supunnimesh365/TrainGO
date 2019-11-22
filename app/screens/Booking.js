ee9772b01ff1f52dd8d03836b8abd0960990083e
supun.n <supunnimesh365@gmail.com>
2019-11-22 22:42:47 +0530

    Finalyzing the booking page 70% done"

--- app/screens/Booking.js
+++ app/screens/Booking.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, StatusBar, Image } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, Image, Picker, ActivityIndicator, TouchableHighlight } from 'react-native';
 import { Slider, Card, ButtonGroup } from 'react-native-elements';
+import firebase from './../constants/firebase';
+import DatePicker from 'react-native-datepicker';
+//import { TouchableHighlight } from 'react-native-gesture-handler';
 // import CalendarPicker from 'react-native-calendar-picker';
+// const data = [
+//   {
+//     "name": "stephen1",
+//     "id": "1234"
+//   },
+//   {
+//     "name": "stephen2",
+//     "id": "1235"
+//   },
+//   {
+//     "name": "stephen3",
+//     "id": "1236"
+//   },
+// ]
 
-
-export default class Booking extends Component {
+class Booking extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,8 +28,111 @@ export default class Booking extends Component {
       Start_App: false,
       successLoad: false,
       selectedStartDate: null,
+      usrid: firebase.auth().currentUser.uid,
+      selectedStationStart: 'Select Starting Station',
+      selectedStationEnd: 'Select Ending Station',
+      selectedTime: [],
+      data1: [],
+      data2: [],
+      data3: [],
+      data4: [],
+      classVal: 3,
+      date: "15-05-2018"
     };
     this.onDateChange = this.onDateChange.bind(this);
+    this.getStationData1 = this.getStationData1.bind(this);
+    this.getStationData2 = this.getStationData2.bind(this);
+    this.selectStart = this.selectStart.bind(this);
+    this.selectEnd = this.selectEnd.bind(this);
+    this.selectTime = this.selectTime.bind(this);
+    this.getTime = this.getTime.bind(this);
+    this.getclass = this.getclass.bind(this);
+  }
+
+
+  selectStart(selectedStationStart) {
+    this.setState({ selectedStationStart })
+    console.log('1111', selectedStationStart)
+  }
+
+  selectEnd(selectedStationEnd) {
+    this.setState({ selectedStationEnd })
+    console.log('2222', selectedStationEnd)
+  }
+
+  selectTime(selectedTime) {
+    this.setState({ selectedTime })
+  }
+
+  getclass(data4) {
+    this.setState({ data4 })
+  }
+
+  getTime(data3) {
+    this.setState({ data3 })
+  }
+
+  getStationData1(data1) {
+    this.setState({ data1 })
+  }
+
+  getStationData2(data2) {
+    this.setState({ data2 })
+  }
+
+  updateClass(classVal) {
+    classVal = classVal + 1
+    this.setState({ classVal })
+  }
+
+  componentDidMount() {
+    var items = []
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '-' + mm + '-' + yyyy;
+    this.setState({ date: today })
+    firebase.database().ref("stations/").on("value", (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+        items.push(childData);
+      })
+      this.getStationData1(items);
+      this.getStationData2(items);
+      this.setState({ successLoad: true });
+
+    }, function (error) {
+      console.log("Error:" + error.code);
+    })
+
+  }
+
+  SampleFunction = (item) => {
+    Alert.alert(item);
+  }
+
+  getAvailableTrains = () => {
+    //get trains
+    //times show
+    //update booked
+    //update seats
+    var items = []
+    console.log('33333', this.state.selectedStationStart, '344444', this.state.selectedStationEnd, '453656');
+    firebase.database().ref("bookingDetails/" + this.state.selectedStationStart + this.state.selectedStationEnd + "/").on("value", (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+        items.push(childData);
+      })
+      this.getTime(items);
+      // this.getclass(items);
+      // console.log(details);
+      //this.setState({ stations: details });
+    }, function (error) {
+      console.log("Error:" + error.code);
+    })
   }
 
   onDateChange(date) {
@@ -25,25 +144,158 @@ export default class Booking extends Component {
   // or authorization number
   render() {
     const { selectedStartDate } = this.state;
+    const { data1, data2, data3, data4 } = this.state;
+    const classes = ['class1', 'class2', 'class3']
+    // console.log('-------',this.state.data1)
     const startDate = selectedStartDate ? selectedStartDate.toString() : '';
-    return (
-      <View style={styles.container}>
-        {/* <CalendarPicker
-          onDateChange={this.onDateChange}
-        /> */}
+    if (!this.state.successLoad) {
+      return (
+        <View style={{ flex: 1 }}>
+          <StatusBar
+            backgroundColor="#ffffff"
+            barStyle="dark-content"
+          />
+          <View>
+            <Image source={require('./../assets/Train05.png')} />
+            <ActivityIndicator size="large" color="blue" />
 
-        <View>
-          <Text>SELECTED DATE:{startDate}</Text>
+          </View>
+
         </View>
-      </View>
-    )
+      );
+    }
+    else {
+      return (
+        <View style={{ flex: 1 }}>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="#ffffff"
+          />
+          <View>
+            <Text>Please select the starting station</Text>
+            <Picker mode="dropdown"
+              selectedValue={this.state.selectedStationStart}
+              onValueChange={(itemValue) =>
+                this.selectStart(itemValue)
+              }
+            >
+              {
+                data1.map((item) => {
+                  return (
+                    <Picker.Item label={item.name} value={item.name + item.id} key={item.name} />
+                  );
+                })
+              }
+            </Picker>
+
+            <Text>Please select the ending station</Text>
+
+            <Picker mode="dropdown"
+              selectedValue={this.state.selectedStationEnd}
+              onValueChange={(itemValue) =>
+                this.selectEnd(itemValue)
+              }
+
+            >
+              {
+                data2.map((item) => {
+                  return (
+                    <Picker.Item label={item.name} value={item.name + item.id} key={item.name} />
+                  );
+                })
+              }
+            </Picker>
+
+
+            <TouchableHighlight
+              onPress={this.getAvailableTrains}
+              style={styles.button}>
+              <Text style={styles.buttontxt}>Get available trains</Text>
+            </TouchableHighlight>
+
+            <Text>Select the date</Text>
+            <DatePicker
+              style={{ width: 200 }}
+              date={this.state.date} //initial date from state
+              mode="date" //The enum of date, datetime and time
+              placeholder="select date"
+              format="DD-MM-YYYY"
+              minDate="01-01-2016"
+              maxDate="01-01-2020"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0
+                },
+                dateInput: {
+                  marginLeft: 36
+                }
+              }}
+              onDateChange={(date) => { this.setState({ date: date }) }}
+            />
+            <Text>Select the time</Text>
+
+            <Picker mode="dropdown"
+              selectedValue={this.state.selectedTime}
+              onValueChange={(itemValue) =>
+                this.selectTime(itemValue)
+              }
+
+            >
+              {
+                data3.map((item) => {
+                  return (
+                    <Picker.Item label={item.time} value={item} key={item.time} />
+                  );
+                })
+              }
+            </Picker>
+
+            <Text>Select the Class</Text>
+
+            <ButtonGroup
+              onPress={this.updateClass}
+              selectedIndex={classVal - 1}
+              buttons={classes}
+              containerStyle={{ height: 65 }}
+            />
+
+            <Text>Seats</Text>
+
+            {/* get these inputs and save it to DB */}
+            {/* my bookings new page */}
+          </View>
+
+        </View>
+      )
+    }
   }
 }
 
+export default Booking;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    marginTop: 100,
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
+  button: {
+    backgroundColor: 'black',
+    height: 70,
+    width: 200,
+    alignSelf: 'center',
+    height: 50,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttontxt: {
+    color: 'white',
+  }
 });
--- app/screens/Wallet.js
+++ app/screens/Wallet.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, StatusBar, Image, ActivityIndicator } from 'rea
 import { Slider, Card, ButtonGroup } from 'react-native-elements';
 import firebase from './../constants/firebase';
 
-export default class Wallet extends Component {
+class Wallet extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,8 +14,8 @@ export default class Wallet extends Component {
     this.updateWallet = this.updateWallet.bind(this)
   }
 
-  updateWallet(Wallet_Ballence){
-    this.setState({Wallet_Ballence})
+  updateWallet(Wallet_Ballence) {
+    this.setState({ Wallet_Ballence })
   }
 
   componentDidMount() {
@@ -27,8 +27,8 @@ export default class Wallet extends Component {
       var newuser = snapshot.val();
       console.log(newuser);
       if (newuser.account_balance < 100) {
-        this.setState({ successLoad: true });
         this.updateWallet(newuser.account_balance);
+        this.setState({ successLoad: true });
         console.log(newuser.account_balance, this.state.Wallet_Ballence)
       }
       else {
@@ -43,44 +43,51 @@ export default class Wallet extends Component {
 
   // viewing balance, history
   render() {
-    const {Wallet_Ballence} = this.state
     // console.log(this.state.Wallet_Ballence);
     if (!this.state.successLoad) {
       return (
-        <View>
+        <View style={{ flex: 1 }}>
           <StatusBar
-            translucent
             backgroundColor="#ffffff"
             barStyle="dark-content"
           />
-          <Image source={require('./../assets/Train05.png')} />
-          <ActivityIndicator size="large" color="blue" />
+          <View>
+            <Image source={require('./../assets/Train05.png')} />
+            <ActivityIndicator size="large" color="blue" />
+
+          </View>
+
         </View>
       );
     }
     else {
       return (
-        <View>
+        <View style={{ flex: 1 }}>
           <StatusBar
-            translucent
             backgroundColor="#ffffff"
             barStyle="dark-content"
           />
-          {/* <Card> */}
-            <Text>{this.state.Wallet_Ballence}</Text>
-            <Text>yyyyyyy</Text>
-          {/* </Card> */}
+          <View>
+            <Card>
+              <Text>{this.state.Wallet_Ballence}</Text>
+              <Text>yyyyyyy</Text>
+            </Card>
+          </View>
+
         </View>
       );
     }
   }
 }
 
+export default Wallet;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    marginTop: 100,
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
 });
 
